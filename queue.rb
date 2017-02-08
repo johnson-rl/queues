32e70cb367aad7f727a7ead99b2061cd91fecbf8
johnson-rl <johnson.rl@gmail.com>
2017-02-08 09:53:12 -0800

    wrote queue methods

--- queue.rb
+++ queue.rb
@@ -1,20 +1,29 @@
+require_relative 'singly_linked_list'
+
 class Queue
 
   attr_reader :front, :contents
 
-  def initialize
+  def initialize(front=nil)
+    @contents = SinglyLinkedList.new front
+    @front = @contents.head
   end
 
-  def enqueue
+  def enqueue element
+    @contents.insert_at_start element
+    @front = @contents.head
   end
 
   def dequeue
+    @contents.remove_from_end
   end
 
   def inspect
+    @contents.inspect
   end
 
   def empty?
+    @contents.empty?
   end
 
 end
